0070ff71bdb2d77c69829fc29c318e68dfabdbb7
Matheus Martinez <decretomusic@gmail.com>
2023-02-14 18:30:18 -0300

    feat: CarController, Routes and App

---
+++ src/Controllers/CarController.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from 'express';
+import ICar from '../Interfaces/ICar';
+import CarService from '../Services/CarService';
+
+class CarController {
+  private req: Request;
+  private res: Response;
+  private next: NextFunction;
+  private carService: CarService;
+
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this.req = req;
+    this.res = res;
+    this.next = next;
+    this.carService = new CarService();
+  }
+
+  public async create() {
+    const car: ICar = {
+      model: this.req.body.model,
+      year: this.req.body.year,
+      color: this.req.body.color,
+      status: this.req.body.status,
+      buyValue: this.req.body.buyValue,
+      doorsQty: this.req.body.doorsQty,
+      seatsQty: this.req.body.seatsQty,
+    };
+
+    try {
+      const newCar = await this.carService.create(car);
+      return this.res.status(201).json(newCar);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+}
+export default CarController;
